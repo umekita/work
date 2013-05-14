cf32922f8cc875ab0b5766452f4b0cccce24f624
umekita <kenichi.umekita@mixi.co.jp>
2013-05-14 09:55:11 +0900

    とちゅうまで

--- practice/fundamentals/6th/ListViewPractice/src/jp/mixi/practice/listview/beg/MainActivity.java
+++ practice/fundamentals/6th/ListViewPractice/src/jp/mixi/practice/listview/beg/MainActivity.java
@@ -1,23 +1,64 @@
-
 package jp.mixi.practice.listview.beg;
+
+import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private Activity mActivity = null;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mActivity = this;
+
+		// ListViewに表示するデータを作成する
+		ArrayList<String> list = new ArrayList<String>();
+		for (int i = 0; i < 20; i++) {
+			list.add("hoge" + i);
+		}
+
+		final ListView listView = (ListView) findViewById(R.id.ListView);
+		CustomListItemAdapter adapter = new CustomListItemAdapter(mActivity, list);
+
+		listView.setAdapter(adapter);
+		// タップした時の動作を定義する
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// Adapterからタップした位置のデータを取得する
+				String str = (String) parent.getItemAtPosition(position);
+				Toast.makeText(mActivity, str, Toast.LENGTH_SHORT).show();
+			}
+		});
+		Button button = (Button) findViewById(R.id.Button);
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				listView.smoothScrollToPosition(0);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 
 }
